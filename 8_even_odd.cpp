a1a071b8c2c19f951770d2848aded633486ba960
Rajkumar-725 <124880996+Rajkumar-725@users.noreply.github.com>
2023-06-21 00:23:59 +0530

    Create 8_even_odd.cpp
    WAP that finds whether a given number is even or odd.

---
+++ 8_even_odd.cpp
@@ -0,0 +1,15 @@
+//WAP that finds whether a given number is even or odd.
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number to check even or odd \n";
+    cin >> n;
+    if(n%2==0)
+    cout<<"Even number";
+    
+    else
+    cout<<"Odd number";
+
+}
