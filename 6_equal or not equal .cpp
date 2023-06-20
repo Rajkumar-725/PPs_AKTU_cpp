e3a2564f004aae743a8de6d558abf210a227e24a
Rajkumar-725 <124880996+Rajkumar-725@users.noreply.github.com>
2023-06-21 00:13:45 +0530

    Create 6_equal or not equal .cpp
    WAP that checks whether the two numbers entered by the user are equal or not.

---
+++ 6_equal or not equal .cpp
@@ -0,0 +1,18 @@
+/*
+WAP that checks whether the two numbers entered by the user are equal or not.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter two numbers \n";
+    cin>>a>>b;
+    if(a==b)
+    cout<<"Equal";
+    else
+    cout<<"Not equal";
+
+    return 0;
+}
